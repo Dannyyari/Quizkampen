32243b4a1492c63c63aab05b6be4871582e9f18e
Danny Yari <danny_yari@yahoo.com>
2024-11-17 16:13:49 +0100

    Ändrat filnamn på gameinterface då det kan förvirras som ett interface. Fixat lite i server, fixat lite i serverlistner som bör vara klar

--- src/ServerSide/ServerListner.java
+++ src/ServerSide/ServerListner.java
@@ -4,14 +4,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerListner {
-    private MultiUser mu=new MultiUser();
 
     public ServerListner() {
         try (ServerSocket serverSocket = new ServerSocket(55555);) {
             while (true){
-                Socket socket= serverSocket.accept();
-                Server ss=new Server(socket, mu);
-                ss.start();
+                Socket socketForPlayer1= serverSocket.accept();
+                System.out.println("en spelare ansluten");
+                Socket socketForPlayer2 = serverSocket.accept();
+                System.out.println("två spelare anslutna, nu kör vi!");
+                Server gameServer= new Server(socketForPlayer1, socketForPlayer2);
+                gameServer.start();
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
